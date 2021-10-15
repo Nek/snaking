c9f3bef24e6ad16e6ec7276f79eb537c00984231
Nikita Dudnik <nikdudnik@gmail.com>
2021-10-15 14:12:46 +0200

    Make it work with the latest prerelease cljs-audio version

--- src/main/com/awayfrompeople/snaking/audio.cljs
+++ src/main/com/awayfrompeople/snaking/audio.cljs
@@ -8,13 +8,14 @@
     [camel-snake-kebab.core :refer [->camelCase]]
     [cljs.core.async :refer [go]]
     [cljs.core.async.interop :refer-macros [<p!]]
-    ["standardized-audio-context" :refer (AudioContext)]))
+    [promesa.core :as p]
+    ["standardized-audio-context" :as sac :refer (AudioContext)]))
 
 (defn graph [{:keys [events ir]}]
-  (let [{:keys [frequency gain]}
-        (s/schedule-parameters
-          {:frequency at-time! :gain (partial adsr! 0.1 0.15 0.6 1 0.2)}
-          events)]
+  (let [params (s/schedule-parameters
+                 {:frequency at-time! :gain (partial adsr! 0.1 0.15 0.6 1 0.2)}
+                 events)
+        {:keys [frequency gain]} params]
     [{:voice  (m/simple-voice
                 {:frequency frequency
                  :gain      gain
@@ -41,45 +42,46 @@
        [:comp :>]
        }]))
 
-(def audio (atom nil))
-
 (def ir (atom nil))
 
 (def events ["touchstart" "touchend" "mousedown" "keydown"])
 
 (def is-initialized (atom false))
 
-(defn update-audio [{:keys [freq time stream ir]}]
-  (when @is-initialized
-    (swap! audio wa/update-audio (graph {:ir ir :events [{:frequency freq :gain 1 ::s/time (+ time 0)}
-                                                         {:frequency (* 2 freq) :gain 1 ::s/time (+ time (t/seconds 120 "1/4"))}
-                                                         {:frequency (* 4 freq) :gain 1 ::s/time (+ time (t/seconds 120 "1/4") (t/seconds 120 "1/4"))}]}))))
+(defn update-audio [{:keys [audio freq time]}]
+  (let [time (+ 0.1 time)]
+    (p/then (wa/calculate-updates @audio (graph {:ir :ir :events [{:frequency freq :gain 1 ::s/time (+ time 0)}
+                                                                  {:frequency (* 2 freq) :gain 1 ::s/time (+ time (t/seconds 120 "1/4"))}
+                                                                  {:frequency (* 4 freq) :gain 1 ::s/time (+ time (t/seconds 120 "1/4") (t/seconds 120 "1/4"))}]}))
+            (fn [updates]
+              (let [new-env (wa/eval-updates! @audio updates)]
+                (reset! audio (into @audio {:env new-env})))))))
+
 
 (defn resume-audio-context []
-  (let [ctx (new AudioContext)]
-    (.then (.resume ctx)
-           (fn []
-             (go
-               (let [;stream (<p! (js/navigator.mediaDevices.getUserMedia (clj->js {:audio true})))
-                     ir-mp3 (<p! (js/fetch "resources/ir.mp3"))
-                     ir-audio-buffer (<p! (.arrayBuffer ir-mp3))
-                     ir-audio-data (<p! (.decodeAudioData ctx ir-audio-buffer))]
-                 (try
-                   (doseq [ev events] (.removeEventListener js/document.body ev resume-audio-context))
-                   (js/document.body.addEventListener "mousedown"
-                                                      (fn [e] (update-audio {:freq (.-offsetY e)
-                                                                             :time (.-currentTime ctx)
-                                                                             :ir   ir-audio-data})))
-                   (js/document.body.addEventListener "touchstart"
-                                                      (fn [e]
-                                                        (let [touch (.item (.-changedTouches e) 0)]
-                                                          (when (not (nil? touch))
-                                                            (update-audio {:freq (.-offsetY e)
-                                                                           :time (.-currentTime ctx)
-                                                                           :ir   ir-audio-data}))
-                                                          ))
-                                                      (reset! audio (wa/make-audio ctx nil))
-                                                      (reset! is-initialized true))
-                   (catch js/Error err (js/console.log (ex-cause err))))))))))
+  (let [audio (atom (wa/make-audio {:polyfill sac}))]
+    (p/then (wa/resume @audio)
+            (fn []
+              (p/let [;stream (<p! (js/navigator.mediaDevices.getUserMedia (clj->js {:audio true})))
+                      ir-audio-data (wa/fetch-sample @audio "resources/ir.mp3")]
+                     (try
+                       (reset! audio (into @audio {:buffers {:ir ir-audio-data}}))
+                       (doseq [ev events] (.removeEventListener js/document.body ev resume-audio-context))
+                       (js/document.body.addEventListener "mousedown"
+                                                          (fn [e] (update-audio {:audio audio
+                                                                                 :freq  (.-offsetY e)
+                                                                                 :time  (wa/current-time @audio)
+                                                                                 })))
+                       (js/document.body.addEventListener "touchstart"
+                                                          (fn [e]
+                                                            (let [touch (.item (.-changedTouches e) 0)]
+                                                              (when (not (nil? touch))
+                                                                (update-audio {:audio audio
+                                                                               :freq  (.-offsetY e)
+                                                                               :time  (wa/current-time @audio)
+                                                                               }))
+                                                              ))
+                                                          (reset! is-initialized true))
+                       (catch js/Error err (js/console.log (ex-cause err)))))))))
 
 (doseq [ev events] (js/document.body.addEventListener ev resume-audio-context false))
