268d63d588cf3e032f1e55b9f4d4aaa69761daa2
Nikita Dudnik <nikdudnik@gmail.com>
2021-10-18 21:10:02 +0200

    Improve scheduling logic

--- src/main/com/awayfrompeople/snaking/audio.cljs
+++ src/main/com/awayfrompeople/snaking/audio.cljs
@@ -24,39 +24,61 @@
     :fx     (m/multi-tap-delay
               {:times (mapv m/at-start
                             [(t/seconds 120 "1/4")
-                             (t/seconds 120 "1/2")
-                             (t/seconds 120 "1")]) :gains (mapv m/at-start [1 0.5 0.25 0.1])})
+                             (t/seconds 120 "1/2")]) :gains (mapv m/at-start [0.5 0.25 0.1])})
     :vca    [:gain {:gain 0.3}]
     :comp   [:dynamics-compressor {:threshold -50 :knee 40 :ratio 12 :attack 0 :release 0.25}]
     }
    #{[:voice2 :vca]
-     ;[:vca :reverb]
-     [:vca :comp]
-     [:voice :fx]
-     [:fx :comp]
-     [:comp :>]
-     }
+     [:voice :vca]
+     [:vca :reverb]
+     [:reverb :comp]
+     [:comp :>]}
    #_#{[:frequency [:voice :osc :frequency]]
-     [:frequency [:voice2 :osc :frequency]]
-     [:gain [:voice :vca :gain]]
-     [:gain [:voice2 :vca :gain]]}])
+       [:frequency [:voice2 :osc :frequency]]
+       [:gain [:voice :vca :gain]]
+       [:gain [:voice2 :vca :gain]]}])
 
 (def ir (atom nil))
 
 (def events ["touchstart" "touchend" "mousedown" "keydown"])
 
+(def bpm 120)
+
+(defn schedule-with-routes! [audio routes path commands]
+  (doseq [ref (filter #(= (first %) path) routes)]
+    (wa/schedule! audio (second ref) commands)))
+
+(def routes #{[[:frequency] [:voice :osc :frequency]]
+              [[:frequency] [:voice2 :osc :frequency]]
+              [[:gain] [:voice :vca :gain]]
+              [[:gain] [:voice2 :vca :gain]]})
+
 (defn schedule-events [{:keys [audio freq]}]
-  (let [time (wa/current-time audio)
-        {:keys [frequency gain]}
-        (s/schedule-parameters
-          {:frequency at-time! :gain (partial l-adsr! 0.1 0.15 0.6 1 0.2)}
-          [{:frequency freq :gain 1 ::s/time (+ time 0)}
-           {:frequency (* 2 freq) :gain 1 ::s/time (+ time (t/seconds 120 "1/4"))}
-           {:frequency (* 4 freq) :gain 1 ::s/time (+ time (t/seconds 120 "1/4") (t/seconds 120 "1/4"))}])]
-    (wa/schedule! audio [:voice :osc :frequency] frequency)
-    (wa/schedule! audio [:voice :vca :gain] gain)
-    (wa/schedule! audio [:voice2 :osc :frequency] frequency)
-    (wa/schedule! audio [:voice2 :vca :gain] gain)))
+  (let [current-time (wa/current-time audio)
+        quarter (t/seconds bpm "1/4")
+        notes [{:pitch 220 :velocity 0.2 :length quarter}
+               {:pitch 440 :velocity 0.1 :length quarter}
+               {:length quarter}
+               {:pitch 236 :velocity 0.3 :length quarter}
+               {:pitch 330 :velocity 0.1 :length quarter}]
+        note->event (fn [current-time {:keys [pitch velocity length] :as note}]
+                      (let [scheduled-time (+ current-time length)]
+                        (if (= 1 (count note))
+                          [scheduled-time nil]
+                          [scheduled-time
+                           [{:frequency pitch
+                            :gain      velocity
+                            ::s/time   scheduled-time}]])))
+        [_ events] (reduce (fn [[current-time events] note]
+                             (let [[scheduled-time event] (note->event current-time note)]
+                               [scheduled-time (into events event)]))
+                           [current-time []]
+                           notes)
+        schedule (s/schedule-parameters
+                   {:frequency at-time! :gain (partial l-adsr! 0.03 0.15 0.7 0.1 0.2)}
+                   events)]
+    (doseq [param-name (keys schedule)]
+      (schedule-with-routes! audio routes [param-name] (param-name schedule)))))
 
 (defn resume-audio-context []
   (let [audio (atom (wa/make-audio {:polyfill sac}))]
